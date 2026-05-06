3c89ff64c1809b6286f9b9cc947b316bc897bf1e
XiaogangWuJNL <782900951@qq.com>
2026-05-06 13:02:19 +0800

    Add files via upload

---
+++ CAG12_for_dbcan.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+for i in $(tail -n+2 /mnt/hwdata/Users/wuxg/Batch_culture/AHL_addition/qsub_sh/dbcan_sh/CAG12_faa.txt | cut -f1); 
+do
+export TMPDIR=/mnt/hwdata/Users/wuxg/tmp/dbCAN
+mamba run -n dbcan_v6 \
+run_dbcan /mnt/hwdata/Users/wuxg/Batch_culture/AHL_addition/faa/CAG12/${i}.faa protein \
+--out_dir /mnt/hwdata/Users/wuxg/Batch_culture/AHL_addition/dbcan/CAG12/${i}__dbcan_results \
+--db_dir /mnt/hwdata/Users/wuxg/database/dbcan414 \
+--tools all
+done
