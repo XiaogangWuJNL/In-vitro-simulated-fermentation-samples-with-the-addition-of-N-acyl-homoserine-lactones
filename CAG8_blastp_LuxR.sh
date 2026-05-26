af7df6f624d4a2d57799adedc4029402fd924d8b
XiaogangWuJNL <782900951@qq.com>
2026-05-26 14:52:02 +0800

    Add files via upload

---
+++ CAG8_blastp_LuxR.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+for i in $(tail -n+2 /mnt/hwdata/Users/wuxg/Batch_culture/AHL_addition/qsub_sh/CAG8_sh/blastp_sh/CAG8_faa.txt | cut -f1); 
+do  
+diamond blastp -d /mnt/hwdata/Users/wuxg/database/BCG_wuxg_DB/LuxR.dmnd \
+-q /mnt/hwdata/Users/wuxg/Batch_culture/AHL_addition/faa/CAG8/${i}.faa \
+-o /mnt/hwdata/Users/wuxg/Batch_culture/AHL_addition/CAG8_results/CAG8_blastp_results/LuxR/${i}_LuxR.out \
+--sensitive -e 1e-5 -f 6 -k 1 --threads 40
+done
+
+
+
